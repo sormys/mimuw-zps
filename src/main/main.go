4fd3c412775b8e5854266fedc4579c743ffeb32f
Szymon Potrzebowski <szymonp1806@gmail.com>
2025-06-23 01:01:16 +0200

    Add flags

--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"io"
 	"log"
 	"log/slog"
 	"mimuw_zps/src/merkle_tree"
@@ -11,17 +13,34 @@ import (
 	"mimuw_zps/src/tui"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/lmittmann/tint"
 )
 
 var nickname string
 
-func setupLogger() {
-	w := os.Stderr
+func parseLogLevel(levelStr string) slog.Level {
+	levelStr = strings.ToUpper(levelStr)
+	switch levelStr {
+	case "DEBUG":
+		return slog.LevelDebug
+	case "INFO":
+		return slog.LevelInfo
+	case "WARN":
+		return slog.LevelWarn
+	case "ERROR":
+		return slog.LevelError
+	default:
+		slog.Warn("Unknown log level, using INFO", "level", levelStr)
+		return slog.LevelInfo
+	}
+}
+
+func setupLogger(logLevel slog.Level, writer io.Writer) {
 	slog.SetDefault(slog.New(
-		tint.NewHandler(w, &tint.Options{
-			Level: slog.LevelDebug,
+		tint.NewHandler(writer, &tint.Options{
+			Level: logLevel,
 			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 				if a.Value.Kind() == slog.KindAny {
 					if _, ok := a.Value.Any().(error); ok {
@@ -34,6 +53,25 @@ func setupLogger() {
 }
 
 func main() {
+	var (
+		logLevelFlag  = flag.String("log-level", "DEBUG", "Log level (DEBUG, INFO, WARN, ERROR)")
+		nicknameFlag  = flag.String("nickname", "Belmondo", "Your nickname for the peer network")
+		logToFileFlag = flag.Bool("log-to-file", false, "Log to file app.log instead of stderr")
+	)
+	flag.Parse()
+
+	logLevel := parseLogLevel(*logLevelFlag)
+	var logWriter io.Writer
+	if *logToFileFlag {
+		var err error
+		logWriter, err = os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatal("Failed to open log file:", err)
+		}
+	} else {
+		logWriter = os.Stderr
+	}
+	setupLogger(logLevel, logWriter)
 
 	waiterCount := uint32(2)
 	senderCount := uint32(2)
@@ -41,13 +79,10 @@ func main() {
 	receiverCount := uint32(2)
 	myAddress := ":0"
 	server_url := "https://galene.org:8448"
-	n := "Urwipołeć śmierdzący groszem"
-
-	setupLogger()
 
 	server := srv_conn.NewServer(server_url)
 
-	nickname = n
+	nickname = *nicknameFlag
 	addr, err := net.ResolveUDPAddr("udp4", myAddress)
 
 	if err != nil {
